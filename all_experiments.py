afcfcb35032516b486b150c9aa5b4eca32da26a0
lotanamit5 <lotan.amit@gmail.com>
2023-06-01 10:22:30 +0300

    Changed epx 1.3 & 1.4
    Co-authored-by: ofernis <ofernis@users.noreply.github.com>

--- all_experiments.py
+++ all_experiments.py
@@ -1,49 +1,49 @@
 from hw2.experiments import cnn_experiment
 
 # experiment 1.1
-for k in [32, 64]:
-    for l in [2, 4, 8, 16]:
-        cnn_experiment(
-            'exp1_1',
-            seed=None,
-            # Training params
-            bs_train=128,
-            bs_test=None,
-            batches=100,
-            epochs=60,
-            early_stopping=3,
-            lr=7e-4,
-            reg=1e-3,
-            # Model Params
-            filters_per_layer=[k],
-            layers_per_block=l,
-            pool_every=min(l//2, 3),
-            hidden_dims=[1024],
-            model_type='cnn', 
-        )
+# for k in [32, 64]:
+#     for l in [2, 4, 8, 16]:
+#         cnn_experiment(
+#             'exp1_1',
+#             seed=None,
+#             # Training params
+#             bs_train=128,
+#             bs_test=None,
+#             batches=100,
+#             epochs=60,
+#             early_stopping=3,
+#             lr=7e-4,
+#             reg=1e-3,
+#             # Model Params
+#             filters_per_layer=[k],
+#             layers_per_block=l,
+#             pool_every=min(l//2, 3),
+#             hidden_dims=[1024],
+#             model_type='cnn', 
+#         )
         
         
 # experiment 1.2
-for l in [2, 4, 8]:
-    for k in [32, 64, 128]:
-        cnn_experiment(
-            'exp1_2',
-            seed=None,
-            # Training params
-            bs_train=128,
-            bs_test=None,
-            batches=100,
-            epochs=60,
-            early_stopping=3,
-            lr=7e-4,
-            reg=1e-3,
-            # Model Params
-            filters_per_layer=[k],
-            layers_per_block=l,
-            pool_every=min(l//2, 3),
-            hidden_dims=[1024],
-            model_type='cnn', 
-        )
+# for l in [2, 4, 8]:
+#     for k in [32, 64, 128]:
+#         cnn_experiment(
+#             'exp1_2',
+#             seed=None,
+#             # Training params
+#             bs_train=128,
+#             bs_test=None,
+#             batches=100,
+#             epochs=60,
+#             early_stopping=3,
+#             lr=7e-4,
+#             reg=1e-3,
+#             # Model Params
+#             filters_per_layer=[k],
+#             layers_per_block=l,
+#             pool_every=min(l//2, 3),
+#             hidden_dims=[1024],
+#             model_type='cnn', 
+#         )
        
         
 # experiment 1.3
@@ -61,7 +61,7 @@ for l in [2, 3, 4]:
         lr=7e-4,
         reg=1e-3,
         # Model Params
-        filters_per_layer=[K],
+        filters_per_layer=K,
         layers_per_block=l,
         pool_every=min(l//2, 3),
         hidden_dims=[1024],
@@ -84,7 +84,7 @@ for K, L in [([32], [8,16,32]),([64, 128, 256], [2,4,8])]:
             lr=7e-4,
             reg=1e-3,
             # Model Params
-            filters_per_layer=[K],
+            filters_per_layer=K,
             layers_per_block=l,
             pool_every=min(l//2, 3),
             hidden_dims=[1024],
